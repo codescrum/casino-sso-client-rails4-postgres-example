4127b50998cbf30aa353dc57d7ed04ae2d239f0d
Juan Pablo Amaya <jp.amaya@codescrum.com>
2015-11-19 10:12:49 -0500

    Change user model to use sso authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 # User model wrapped by the devise gem
 class User < ActiveRecord::Base
-
+  include Devise::Models::DatabaseAuthenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :cas_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, presence: true, uniqueness: true
 end
